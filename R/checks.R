1b05351c14c4ec01619b95ce83d4f486d7f74f8e
David Ranzolin <daranzolin@gmail.com>
2023-02-28 09:44:30 -0800

    cleanup

--- R/checks.R
+++ R/checks.R
@@ -1,5 +1,9 @@
-## Checks ##
-
+#' Compare the origins of two qtr objects.
+#'
+#' @param x an object of class qtr.
+#' @param y an object of class qtr.
+#'
+#' @return logical
 #' @export
 origins_check <- function(x, y) {
   if (!identical(qtr_origin(x), qtr_origin(y))) {
